542c1180da49a3dc96ef3dce3b617715347e3422
stevenburris1978 <s_burris@mail.fhsu.edu>
2022-10-29 20:57:50 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {     
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => {
+          console.log(`Service Worker Registration (Scope: ${reg.scope})`);
+        })
+        .catch((error) => {
+          console.log(`Service Worker Error (${error})`);
+        });
+    });
+  } else {
+    console.log("Service Worker not available");
+  }
